01566a55471ca36f9b286c6e778230ff9f4c4f6b
Steven Hickson <me@stevenhickson.com>
2018-02-19 10:49:51 -0500

    Added instance generation stub

---
+++ generate_instances.cpp
@@ -0,0 +1,16 @@
+// Genereates instance labels using our proposal method.
+//
+
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
