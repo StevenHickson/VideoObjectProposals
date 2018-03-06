2ecb96b73618f1d705497231b45817501b5551eb
StevenHickson <me@stevenhickson.com>
2018-03-06 18:41:02 -0500

    Updated generate proposals to near working

--- generate_proposals.cpp
+++ generate_proposals.cpp
@@ -13,9 +13,13 @@
 #include <gflags/gflags.h>
 #include "image_extractor.h"
 #include "conversions.h"
+#include "point_cloud_ops.h"
+#include "city_scape_info.h"
 
 
 DEFINE_string(file_list, "", "The list of files to parse.");
+
+DEFINE_string(output_folder, "proposals", "The output folder name");
 
 DEFINE_int32(min_size, 64,
              "The minimum width and height for an instance mask.");
@@ -28,12 +32,7 @@ DEFINE_int32(output_width, 512,
              "The size of the network input window. "
              "All output images will be resized to that before saving.");
 
-DEFINE_enum(
-    string, object_subset, "NORMAL",
-    "Which split of true labels to use as samples. Allowed modes:\n  NORMAL: "
-    "This is the full cityscapes split.\n  SMALL: This is just car, bicycle, "
-    "and person.\n  UNSUP: This is the unsupervised version.",
-    "UNSUP", "SMALL", "UNSUP");
+DEFINE_int32(object_subset, 0, "0 == normal, 1 == small, 2 == unsupervised.");
 
 DEFINE_bool(background_patches, false,
             "If true, will generate random background patches.");
@@ -52,7 +51,7 @@ DEFINE_int32(patch_min_size, 64,
 DEFINE_int32(patch_max_size, 64,
              "The maximum width and height for a negative patch.");
 
-DEFINE_int32(num_patches, 20, "The number of negative patches to generate.");
+DEFINE_int32(num_patches, 10, "The number of negative patches to generate.");
 
 DEFINE_int32(jitter, 0,
              "The amount (+/-) to jitter the bounding boxes of samples "
@@ -69,6 +68,8 @@ DEFINE_double(optical_flow_dist, 10, "The optical flow distance.");
 
 using namespace cv;
 using namespace std;
+using namespace unsup_clustering;
+using namespace cityscape;
 
 
 void GetOpticalFlow(const Mat& past_image, const Mat& current_image,
@@ -77,7 +78,7 @@ void GetOpticalFlow(const Mat& past_image, const Mat& current_image,
   pcops.ComputeOpticalFlow(past_image, current_image, flow);
 }
 
-void CalcBackgroundFlow(const Mat& instange_image, const Mat& flow,
+void CalcBackgroundFlow(const Mat& instance_image, const Mat& flow,
                         Vec2f* background_flow) {
   Mat_<Vec2f>::const_iterator pF = flow.begin<Vec2f>();
   Mat_<uchar>::const_iterator pI = instance_image.begin<uchar>();
@@ -96,13 +97,97 @@ void CalcBackgroundFlow(const Mat& instange_image, const Mat& flow,
 float CalcFlowDistance(const Mat& flow, const Rect& roi,
                        const Vec2f& background_flow) {
   Mat roiMat = flow(roi);
-  Scalar mean = mean(roiMat);
+  Scalar mean = cv::mean(roiMat);
   if (roiMat.empty()) {
     cout << "Couldn't calc flow dist" << endl;
     return 0;
   }
   return std::abs(mean[0] - background_flow[0]) +
          std::abs(mean[1] - background_flow[1]);
+}
+
+class Generator {
+  public:
+    Generator() { };
+    void SaveData(const Mat &img, const Rect &roi, const int label, const int count, const string &filename);
+    void CreateExamplesFromInstances(
+                  const Mat& input_image, const Mat& instance_images,
+                  const std::map<int, ObjectInfo>& instance_map, const string& filename);
+
+    ofstream output_file_;
+    Mat flow_;
+    Mat background_flow_;
+    Extractor extractor_;
+};
+
+void Generator::SaveData(const Mat &img, const Rect &roi, const int label, const int count, const string &filename) {
+  // Save the image in the proper directory, that is dictated by label
+  int save_label;
+  if (FLAGS_object_subset == 1) {
+    save_label = GetTargetSubsetObjectIds(label);
+  } else if (FLAGS_object_subset == 0) {
+    save_label = GetTargetObjectIds(label);
+  } else {
+    if (label == 0)
+      save_label = 0; 
+    else if (label == 1 || label == 255)
+      save_label = 1; 
+    else
+      cout << "Erroronous label!" << endl;
+  }
+  string folder;
+  if (save_label == 0)
+    folder = "background";
+  else
+    folder = "foreground";
+  stringstream save_name;
+  save_name << "/data/" << FLAGS_output_folder << "/" << folder << "/" << count << ".png";
+  imwrite(save_name.str(), img);
+  output_file_ << save_name.str() << "," << filename << "," << label << "," << save_label << ",";
+  output_file_ << roi.x << "," << roi.y << "," << roi.width << "," << roi.height << "\n";
+}
+
+
+void Generator::CreateExamplesFromInstances(
+    const Mat& input_image, const Mat& instance_images,
+    const std::map<int, ObjectInfo>& instance_map, const string& filename) {
+
+  std::map<int, ObjectInfo> allInstances;
+  std::vector<int> positiveLabels;
+  if (FLAGS_object_subset == 0)
+    positiveLabels = {11, 12, 13, 14, 15, 16, 17, 18};
+  else if (FLAGS_object_subset == 1)
+    positiveLabels = {11, 13, 18};
+  else
+    positiveLabels = {1};
+
+  extractor_.CreateNegativePatches(input_image.cols, input_image.rows,
+                                   FLAGS_patch_min_size, FLAGS_patch_max_size,
+                                   FLAGS_num_patches, positiveLabels,
+                                   instance_map, &allInstances);
+
+  std::vector<ObjectInfo> croppedInstanceImages;
+  extractor_.CreateDataFromInstances(
+      input_image.cols, input_image.rows, FLAGS_output_width,
+      FLAGS_output_height, FLAGS_min_size, FLAGS_max_aspect_ratio,
+      FLAGS_keep_aspect_ratio, allInstances, &croppedInstanceImages);
+
+
+  std::vector<ObjectInfo>::const_iterator pI = croppedInstanceImages.begin();
+
+  int c = 0;
+  while (pI != croppedInstanceImages.end()) {
+    Rect roi(pI->x_min_, pI->y_min_, pI->x_max_, pI->y_max_);
+    if (pI->label_ == 0 || !FLAGS_use_optical_flow ||
+        CalcFlowDistance(flow_, roi, background_flow_) >= FLAGS_optical_flow_dist) {
+
+      Mat cropped_image = input_image(roi).clone();
+      Mat cropped_instance_image = instance_images(roi).clone();
+
+      SaveData(cropped_image, roi, pI->label_, c, filename);
+    }
+    ++pI; ++c;
+  }
 }
 
 int main(int argc, char* argv[]) {
@@ -116,9 +201,55 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
+  Generator gen;
+  gen.output_file_.open("/data/" + FLAGS_output_folder + "/info.txt", ios::out);
+
   string line;
   while(getline(in, line)) {
+    string disp_name, inst_name, label_name;
+    Mat image, instance_img, label_img;
+    cout << "Parsing: " << line << endl;
+
+    image = imread(line, CV_LOAD_IMAGE_COLOR);
+    if (image.empty()) {
+      cout << "Error, couldn't load image" << endl;
+      break;
+    }
+
+    bool read_inst_16bit = false;
+    if (FLAGS_object_subset == 2) {
+      inst_name = regex_replace(line, regex("png"), "inst.png");
+      label_name = regex_replace(line, regex("png"), "label.png");
+    } else {
+      inst_name = regex_replace(line, regex("_leftImg8bit"), "_gtFine_instanceIds");
+      inst_name = regex_replace(inst_name, regex("leftImg8bit"), "gtFine");
+      label_name = regex_replace(line, regex("_leftImg8bit"), "_gtFine_labelIds");
+      label_name = regex_replace(label_name, regex("leftImg8bit"), "gtFine");
+      read_inst_16bit = true;
+    }
+
+    if (read_inst_16bit)
+      instance_img = imread(inst_name, CV_LOAD_IMAGE_ANYDEPTH);
+    else
+      instance_img = imread(inst_name, CV_LOAD_IMAGE_GRAYSCALE);
+    if (instance_img.empty()) {
+      cout << "Error, couldn't load instance" << endl;
+      break;
+    }
     
+    label_img = imread(label_name, CV_LOAD_IMAGE_GRAYSCALE);
+    if (label_img.empty()) {
+      cout << "Error, couldn't load label" << endl;
+      break;
+    }
+    
+    std::map<int, ObjectInfo> instanceMap;
+    bool fully_unsup = false;
+    if (FLAGS_object_subset == 2)
+      fully_unsup = true;
+    gen.extractor_.ExtractInstanceData(image, label_img, instance_img, fully_unsup, &instanceMap);
+    gen.CreateExamplesFromInstances(image, instance_img, instanceMap, line);
   }
+  gen.output_file_.close();
   return 0;
 }
