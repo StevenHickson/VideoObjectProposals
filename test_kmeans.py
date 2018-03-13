da25bc01fb6d8bdc1b32b2f7eb2ae022cd9a0b45
Steven Hickson <me@stevenhickson.com>
2018-03-12 20:32:49 -0400

    Updated test_kmeans to use gflags and be much better

--- test_kmeans.py
+++ test_kmeans.py
@@ -13,17 +13,19 @@ from sklearn.metrics.cluster import adjusted_mutual_info_score
 # Input is the train set, the validation set, # of clusters
 # An example is python test_kmeans.py /data/gt_proposals/embeddings_baseline_train.npy /data/gt_proposals/embeddings_baseline.npy
 
-train_set = np.load(sys.argv[1])
-val_set = np.load(sys.argv[2])
-num_k = int(sys.argv[3])
+tf.app.flags.DEFINE_string(
+    'train_file', '', 'The train_embeddings to use.')
 
-clusterer = kmeans.KMeansClustering(
-      num_clusters=num_k,
-      use_mini_batch=False)
-x = train_set
-labels = x[:,0:1]
-ks = x[:1:2]
-x = x[:,2:]
+tf.app.flags.DEFINE_string(
+    'test_file', '', 'The test_embeddings to use.')
+
+tf.app.flags.DEFINE_integer(
+    'num_k', 3, 'The number of k clusters to use.')
+
+tf.app.flags.DEFINE_bool(
+    'endtoend', False, 'Whether to use endtoend k clusters.')
+
+FLAGS = tf.app.flags.FLAGS
 
 def _input_fn(tensor, num_epochs=None):
     """Constructs an input_fn for KMeansClustering estimator.
@@ -42,52 +44,64 @@ def _input_fn(tensor, num_epochs=None):
           None)
     return _constant_input_fn
 
-clusterer.fit(
-      input_fn=_input_fn(x.astype(np.float32)), steps=10)
+def process_clusters(name, labels, predictions):
+    cluster_counts = dict()
+    for l,p in zip(labels,predictions):
+        if l not in cluster_counts:
+            cluster_counts[l] = [0] * FLAGS.num_k
+        cluster_counts[l][p] += 1
 
-x_val = val_set
-labels_val = x_val[:,0:1]
-ks_val = x_val[:,1:2]
-x_val = x_val[:,2:]
-predictions = np.array(list(clusterer.predict_cluster_idx(
-        input_fn=_input_fn(x_val.astype(np.float32), num_epochs=1))))
-
-cluster_counts = dict()
-for l,p in zip(labels_val.flatten(),predictions):
-    if l not in cluster_counts:
-        cluster_counts[l] = [0] * num_k
-    cluster_counts[l][p] += 1
-
-cluster_counts_ete = dict()
-for l,p in zip(labels_val.flatten(),ks_val.flatten()):
-    if l not in cluster_counts_ete:
-        cluster_counts_ete[l] = [0] * num_k
-    cluster_counts_ete[l][int(p)] += 1
-
-#print(cluster_counts)
-minVal = min(cluster_counts)
-maxVal = max(cluster_counts)
-if minVal < 11 or maxVal > 18:
-    origId = True
-else:
-    origId = False
-print('\n----------K Cluster results----------\n\n')
-for k,v in cluster_counts.iteritems():
-    if origId:
-        print(OriginalIdToName[k] + ': ' + str(v))
+    minVal = min(cluster_counts)
+    maxVal = max(cluster_counts)
+    if minVal < 11 or maxVal > 18:
+        origId = True
     else:
-        print(TrainIdToName[k] + ': ' + str(v))
+        origId = False
+    print('\n----------' + name + ' results----------\n\n')
+    for k,v in cluster_counts.iteritems():
+        if origId:
+            print(OriginalIdToName[k] + ': ' + str(v))
+        else:
+            print(TrainIdToName[k] + ': ' + str(v))
 
-print('\n----------K end-to-end results----------\n\n')
-for k,v in cluster_counts_ete.iteritems():
-    if origId:
-        print(OriginalIdToName[k] + ': ' + str(v))
+    print('\nNormalized Mutual Information Score')
+    print normalized_mutual_info_score(labels,predictions)
+
+    print('\nAdjusted Mutual Information Score')
+    print adjusted_mutual_info_score(labels,predictions)
+
+def main(_):
+    train_set = np.load(FLAGS.train_file)
+    val_set = np.load(FLAGS.test_file)
+    num_k = int(FLAGS.num_k)
+
+    clusterer = kmeans.KMeansClustering(
+          num_clusters=num_k,
+          use_mini_batch=False)
+    labels = train_set[:,0:1]
+    if FLAGS.endtoend:
+        ks = train_set[:1:2]
+        x = train_set[:,2:]
     else:
-        print(TrainIdToName[k] + ': ' + str(v))
+        x = train_set[:,1]
 
-print('\nNormalized Mutual Information Score')
-print normalized_mutual_info_score(labels_val.flatten(),predictions)
+    clusterer.fit(
+          input_fn=_input_fn(x.astype(np.float32)), steps=10)
 
-print('\nAdjusted Mutual Information Score')
-print adjusted_mutual_info_score(labels_val.flatten(),predictions)
+    labels_val = val_set[:,0:1]
+    if FLAGS.endtoend:
+        ks_val = val_set[:,1:2]
+        x_val = val_set[:,2:]
+    else:
+        x_val = val_set[:,1:]
+    predictions = np.array(list(clusterer.predict_cluster_idx(
+            input_fn=_input_fn(x_val.astype(np.float32), num_epochs=1))))
 
+    process_clusters('K Cluster', labels_val.flatten(), predictions)
+
+    if FLAGS.endtoend:
+        process_clusters('K end-to-end', labels_val.flatten(), ks_val.flatten().astype(int))
+
+
+if __name__ == '__main__':
+    tf.app.run()
